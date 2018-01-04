672044a074b00884495247417443c742b4c1435d
Vladimir <vovnit@phie.ru>
2018-01-04 23:26:40 +0300

    a little refactored, needs more

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
-# TODO refactor code
-import json
 import logging
 import os
-import tempfile
-import urllib.request
-from multiprocessing.pool import ThreadPool
 
 import cherrypy
 import cv2
-import numpy
 import telebot
-from telebot import types
 
 import config
 from detector import Detector
+from processing import prepare_url, url_to_cv2, process_photo_message
 from user import user
 
+# initialization
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 bot = telebot.TeleBot(config.token)
@@ -42,6 +37,7 @@ detector = Detector()
 users = []
 
 
+#handlers
 @bot.message_handler(content_types=["text"])
 def repeat_all_text(message):
     pass
@@ -57,45 +53,14 @@ def photo(message, is_callback=False):
         users.append(cur_user)
     if is_callback:
         print(str(cur_user.chat_id) + 'started working on a picture in current thread')
-        while not process_photo_message(message, cur_user) and cur_user.tries <= len(detector.haarcascades):
+        while not process_photo_message(message, cur_user, detector, bot) and cur_user.tries <= len(
+                detector.haarcascades):
             pass
     else:
-        pool = ThreadPool(processes=1)
         print(str(cur_user.chat_id) + ' started working on a picture in a new thread')
-        async_result = pool.apply_async(process_photo_message, args=(message, cur_user))
-        async_result.get()
+        process_photo_message(message, cur_user, detector, bot)
 
 
-def process_photo_message(message, usr):
-    cv_mat = url_to_cv2(prepare_url(message))
-
-    cv_mat = detector.detect_head(cv_mat, usr)
-
-    tmp_file = tempfile.TemporaryFile("w+b")
-    if cv_mat is not None:
-        print(str(usr.chat_id) + ', face was found, will send it to user, try #' + str(usr.tries))
-        encoded_image = cv2.imencode(ext='.png', img=cv_mat)[1]
-        tmp_file.write(encoded_image)
-        keyboard = types.InlineKeyboardMarkup()
-        callback_true = types.InlineKeyboardButton(text="Всё верно", callback_data="true")
-        callback_false = types.InlineKeyboardButton(text="Есть ошибка", callback_data="false")
-        keyboard.add(callback_true)
-        keyboard.add(callback_false)
-        tmp_file.seek(0)
-        bot.send_photo(message.chat.id, tmp_file, reply_markup=keyboard)
-        detector.default_haarcascade_for_user(usr)
-        tmp_file.close()
-    elif usr.tries >= len(detector.haarcascades) - 1 or usr.tries >= len(detector.haarcascades) - 1 and cv_mat is None:
-        print(str(usr.chat_id) + ' exceeded his tries and face wasn\'t found try #' + str(usr.tries))
-        usr.tries = 0
-        detector.default_haarcascade_for_user(usr)
-        bot.send_message(message.chat.id, "Лицо не найдено, попробуйте другую фотографию")
-        tmp_file.close()
-    elif cv_mat is None:
-        print(str(usr.chat_id) + ', face wasn\'t found try #' + str(usr.tries))
-        detector.next_haarcascade_for_user(usr)
-        tmp_file.close()
-        process_photo_message(message, usr)
 
 
 # В большинстве случаев целесообразно разбить этот хэндлер на несколько маленьких
@@ -105,13 +70,11 @@ def callback_inline(call):
     chat_id = call.message.chat.id
     # Если сообщение из чата с ботом
     if call.message:
-
         cur_user: user = next((usr for usr in users if usr.chat_id == call.message.chat.id), False)
         if not cur_user:
             return
         if call.data == "true":
             print(str(cur_user.chat_id) + ' accepted our cropping')
-            bot.edit_message_reply_markup(chat_id, call.message.message_id)
             path = '/root/profile_pics/' + str(call.message.chat.id) + '.png'
             try:
                 os.remove(path)
@@ -128,7 +91,6 @@ def callback_inline(call):
             print(str(cur_user.chat_id) + ' didn\'t accept our cropping, we will try again or stop')
             if (cur_user.tries + 1 >= len(detector.haarcascades)):
                 bot.send_message(chat_id, "К сожалению лицо не было найдено! Может попробуем другую фотографию?")
-                bot.edit_message_reply_markup(chat_id, call.message.message_id)
 
                 users.remove(cur_user)
                 # bot.edit_message_text(text='', chat_id=chat_id, message_id=call.message.message_id)
@@ -138,33 +100,10 @@ def callback_inline(call):
                 res_user = next(usr for usr in users if usr.chat_id == chat_id)
                 detector.next_haarcascade_for_user(res_user)
                 process_photo_message(call.message, cur_user)
+        bot.edit_message_reply_markup(chat_id, call.message.message_id)
 
 
-def prepare_url(msg):
-    file_id_url = 'https://api.telegram.org/bot<bot_token>/getFile?file_id=<the_file_id>'
-    result = json.load(urllib.request.urlopen(file_id_url
-                                              .replace('<bot_token>',
-                                                       str(config.token))
-                                              .replace('<the_file_id>',
-                                                       str(msg.photo[len(msg.photo) - 1].file_id))))['result']
-    file_path = result['file_path']
-    file_path_url = 'https://api.telegram.org/file/bot<token>/<file_path>'
-    return file_path_url \
-        .replace('<token>', config.token) \
-        .replace('<file_path>', file_path)
-
-
-def url_to_cv2(url):
-    result = url_to_image(url)
-    image = numpy.asarray(bytearray(result), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    return image
-
-
-def url_to_image(url):
-    response = urllib.request.urlopen(url)
-    return response.read()
-
+# starting service
 
 bot.remove_webhook()
 bot.set_webhook(url=config.WEBHOOK_URL_BASE + config.WEBHOOK_URL_PATH,
---
+++ processing.py
@@ -0,0 +1,67 @@
+import json
+import tempfile
+import urllib.request
+
+import cv2
+import numpy
+from telebot import types
+
+import config
+
+
+def prepare_url(msg):
+    file_id_url = 'https://api.telegram.org/bot<bot_token>/getFile?file_id=<the_file_id>'
+    result = json.load(urllib.request.urlopen(file_id_url
+                                              .replace('<bot_token>',
+                                                       str(config.token))
+                                              .replace('<the_file_id>',
+                                                       str(msg.photo[len(msg.photo) - 1].file_id))))['result']
+    file_path = result['file_path']
+    file_path_url = 'https://api.telegram.org/file/bot<token>/<file_path>'
+    return file_path_url \
+        .replace('<token>', config.token) \
+        .replace('<file_path>', file_path)
+
+
+def url_to_cv2(url):
+    result = url_to_image(url)
+    image = numpy.asarray(bytearray(result), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+
+def url_to_image(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+
+def process_photo_message(message, usr, detector, bot):
+    cv_mat = url_to_cv2(prepare_url(message))
+
+    cv_mat = detector.detect_head(cv_mat, usr)
+
+    tmp_file = tempfile.TemporaryFile("w+b")
+    if cv_mat is not None:
+        print(str(usr.chat_id) + ', face was found, will send it to user, try #' + str(usr.tries))
+        encoded_image = cv2.imencode(ext='.png', img=cv_mat)[1]
+        tmp_file.write(encoded_image)
+        keyboard = types.InlineKeyboardMarkup()
+        callback_true = types.InlineKeyboardButton(text="Всё верно", callback_data="true")
+        callback_false = types.InlineKeyboardButton(text="Есть ошибка", callback_data="false")
+        keyboard.add(callback_true)
+        keyboard.add(callback_false)
+        tmp_file.seek(0)
+        bot.send_photo(message.chat.id, tmp_file, reply_markup=keyboard)
+        detector.default_haarcascade_for_user(usr)
+        tmp_file.close()
+    elif usr.tries >= len(detector.haarcascades) - 1 or usr.tries >= len(detector.haarcascades) - 1 and cv_mat is None:
+        print(str(usr.chat_id) + ' exceeded his tries and face wasn\'t found try #' + str(usr.tries))
+        usr.tries = 0
+        detector.default_haarcascade_for_user(usr)
+        bot.send_message(message.chat.id, "Лицо не найдено, попробуйте другую фотографию")
+        tmp_file.close()
+    elif cv_mat is None:
+        print(str(usr.chat_id) + ', face wasn\'t found try #' + str(usr.tries))
+        detector.next_haarcascade_for_user(usr)
+        tmp_file.close()
+        process_photo_message(message, usr, detector, bot)
